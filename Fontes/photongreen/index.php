ba5447c860b3fe7dbce5ef017e5816b8d4eaa564
nosrednawall <nosrednawall@gmail.com>
2018-08-16 19:57:43 -0300

    adicionado bootstrap ao projeto

---
+++ Fontes/photongreen/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Photon Green</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="ressources/css/bootstrap.min.css" />
+    <script src="resources/js/bootstrap.bundle.js"></script>
+</head>
+<body>
+<header class="container"></header>
+<article class="container"></article>
+<footer class="container">
+        Photon Green v001
+</footer>    
+</body>
+</html>
