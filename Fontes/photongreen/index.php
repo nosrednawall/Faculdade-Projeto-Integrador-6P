654842ff2b15a768bc5ad6144ffec2602fbb0a1b
nosrednawall <nosrednawall@gmail.com>
2018-08-18 13:51:29 -0300

    adicionado arquivo astah

--- Fontes/photongreen/index.php
+++ Fontes/photongreen/index.php
@@ -5,14 +5,27 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>Photon Green</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="ressources/css/bootstrap.min.css" />
-    <script src="resources/js/bootstrap.bundle.js"></script>
+    <link rel="stylesheet" type="text/css" media="screen" href="/ressources/css/bootstrap.min.css" />
+    <script src="/resources/js/bootstrap.bundle.js"></script>
 </head>
 <body>
-<header class="container"></header>
-<article class="container"></article>
+<header class="container">
+    <div class="row">
+
+    </div>
+</header>
+<article class="container">
+    <div class="row">
+        <div class="col"><div class="jumbontron"><h1>Titulo Site</h1></div></div>
+    </div>
+    <div class="row">
+        <div class="col"></div>
+    </div>
+</article>
 <footer class="container">
+    <div class="row">
         Photon Green v001
+    </div>
 </footer>    
 </body>
 </html>
