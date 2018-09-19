0f6f735cd93c9f705ddb03109360ae5e058c3170
nosrednawall <nosrednawall@gmail.com>
2018-09-18 21:25:48 -0300

    adicionado os arquivos de css e js ao projeto

--- Fontes/photongreen/index.php
+++ Fontes/photongreen/index.php
@@ -4,12 +4,10 @@
 	<title>PhotonGreen</title>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
-	<meta charset="utf-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	<link rel="stylesheet" href="css/layout.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<script src="js/jquery.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
 
 <body>
 	<?php include("template/cabecalho.php"); ?>
