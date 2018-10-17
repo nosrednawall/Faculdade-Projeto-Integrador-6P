fec4f2425bbd42c72c0b2820bcccf745e03affcd
nosrednawall <nosrednawall@gmail.com>
2018-10-16 21:49:08 -0300

    adicionado google analytics

--- Fontes/photongreen/template/rodape.php
+++ Fontes/photongreen/template/rodape.php
@@ -7,4 +7,14 @@
 	<p>photongreen.com.br<a href="https://www.w3schools.com/w3css/default.asp" title="W3.CSS" target="_blank" class="w3-hover-text-green"></a></p>
 </footer>
 
+<!-- Scripts adicionais -->
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-127321879-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'UA-127321879-2');
+</script>
