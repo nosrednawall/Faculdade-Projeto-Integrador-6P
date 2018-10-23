5e9af0880611358c134dfc7251eb99b23df7141b
nosrednawall <nosrednawall@gmail.com>
2018-10-23 17:50:44 -0300

    adicioando uma imagen como pano de fundo que ainda não funciona

--- Fontes/photongreen/calcular.php
+++ Fontes/photongreen/calcular.php
@@ -6,14 +6,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="stylesheet" href="css/layout.css">
 	<link rel="stylesheet" href="css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="css/calcular.css"/>
+
 	<script src="js/jquery.min.js"></script>
 	<script src="js/bootstrap.min.js"></script>
-	
 	<script type="text/javascript" src="./js/angular.min.js"></script>
 	<script type="text/javascript" src="./js/jquery.min.js"></script>
-	
 	<script type="text/javascript" src="./js/calculo.js"></script>	
-	<link rel="stylesheet" type="text/css" href="calcular.css"/>
+
 	
 <body ng-controller="myCtrl">
 
