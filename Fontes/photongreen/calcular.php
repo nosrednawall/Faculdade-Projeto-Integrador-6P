63bfa175d3de6a81186c99f9aa63b0aa2afaa95f
nosrednawall <nosrednawall@gmail.com>
2018-11-13 09:17:21 -0200

    ok

--- Fontes/photongreen/calcular.php
+++ Fontes/photongreen/calcular.php
@@ -2,7 +2,8 @@
 <html ng-app="myApp">
 <head>
 	<title>Calculo</title>
-	<meta charset="UTF-8">
+	<meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
     <?php include("template/css.php"); ?>
 </head>
