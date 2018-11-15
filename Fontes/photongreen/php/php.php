ef1852782ccdae2bb9e768ccd8ecd6e49625ddf0
nosrednawall <nosrednawall@gmail.com>
2018-11-15 11:11:56 -0200

    removido alguns arquivos de testes e renomeado o solver.py para solver-preliminar.py

--- Fontes/photongreen/php/php.php
+++
@@ -1,7 +0,0 @@
-<?php 
-
- $p = "ola mundo";
- $pyscript = 'py.py';
- $run = shell_exec("python py.py $p");
-
-?>
--- Fontes/photongreen/php/solver.php
+++ Fontes/photongreen/php/solver.php
@@ -29,7 +29,7 @@
         $resultadoPlaca325 = resolverSolver($painel325, $meta_energia,$area_informada,$valor_maximo);
         $resultadoPlaca330 = resolverSolver($painel330, $meta_energia,$area_informada,$valor_maximo);
 
-    
+
         //gera o json de retorno para o javascript
         $saida = array(
             '250' => $resultadoPlaca250,
@@ -50,10 +50,17 @@
     
     //funcao que efetua o solver enviando os dados para o script em python
     function resolverSolver($painelEscolhido,$meta_energia,$area_informada,$valor_maximo){
-        $solucao = shell_exec('python solver.py '. $valor_maximo .' '.$painelEscolhido->preco.' '.$area_informada.' '.
+        $solucao = shell_exec('python solver-preliminar.py '. $valor_maximo .' '.$painelEscolhido->preco.' '.$area_informada.' '.
                                 $painelEscolhido->tamanho_painel.' '.$meta_energia.' '.$painelEscolhido->potencia);
         return $solucao;
     }
+
+
+
+
+
+
+
 
     //historico de tentativas
 
@@ -95,4 +102,35 @@
 
     // envia os dados para o javascript via ajax
 
+
+    // require_once 'painel-solar.php';
+ 
+// $painel = new PainelSolar("330");
+// echo $painel->potencia;
+// echo $painel->descricao;
+// echo $painel->altura;
+// echo " ";
+// echo $painel->preco;
+
+// class AcessaPainel{
+//     function imprimeUsuario(){
+//         $painel = new PainelSolar("330");
+//         echo $painel->potencia;
+//         echo $painel->descricao;
+//         echo $painel->altura;
+//     }
+// }
+
+// $valor_maximo = 15000;
+// $valor_painel = 690;
+// $area_informada = 20;
+// $tamanho_painel = 1.6;
+// $meta_energia = 2000;
+// $potencia_painel = 250;
+
+// $resultadoEmJson = shell_exec('python solver.py '. $valor_maximo .' '.$valor_painel.' '.$area_informada.' '.
+//                                 $tamanho_painel.' '.$meta_energia.' '.$potencia_painel);
+// echo $resultadoEmJson;
+// $resultado = json_decode($resultadoEmJson);
+// echo"$resultado";
  ?>
