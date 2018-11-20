4183adf5bc1a254d8784c9a60e1139adf5bbdd65
nosrednawall <nosrednawall@gmail.com>
2018-11-20 13:21:40 -0200

    removido o erro chato do favicon

--- Fontes/photongreen/index.php
+++ Fontes/photongreen/index.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<?php include("template/css.php"); ?>
-
+	<link rel="shortcut icon" href="#">
 <head>
 <body>
 	<?php include("template/menu.php"); ?>
